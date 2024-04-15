d52b2aa096754104171be0bc96eefadad8f042fc
Max Görner <5477952+MaxG87@users.noreply.github.com>
2024-04-15 23:35:14 +0200

    feat: Add reverse_multimapping
    A last handy function is added. It will reverse a multimapping to a
    collectiondict.

---
+++ tests/test_reverse_multimapping.py
@@ -0,0 +1,96 @@
+import typing as t
+from collections import Counter, deque
+from itertools import groupby
+
+import pytest
+from hypothesis import given
+from hypothesis import strategies as st
+
+from collectiondict import reverse_multimapping
+from tests import custom_classes as cc
+from tests import hypothesis_utils as hu
+
+
+def valid_mappings() -> st.SearchStrategy[dict[int, list[int]]]:
+    # We take the modulo 13 to provoke reuse of values. This will force the
+    # implementation to create collections with multiple keys correctly.
+    return st.dictionaries(st.integers(), st.lists(st.integers().map(lambda x: x % 13)))
+
+
+@given(mapping=valid_mappings(), clct=hu.valid_ordered_collections())
+def test_reverse_multimapping_for_ordered_collections(
+    mapping: dict[int, list[int]],
+    clct: t.Union[
+        t.Type[cc.MyList[int]],
+        t.Type[cc.MyTuple[int]],
+        t.Type[list[int]],
+        t.Type[tuple[int, ...]],
+    ],
+) -> None:
+    result = reverse_multimapping(clct, mapping)
+    grouped = groupby(
+        sorted(
+            ((key, val) for key, values in mapping.items() for val in values),
+            key=lambda x: x[1],
+        ),
+        lambda x: x[1],
+    )
+
+    expected = {}
+    for val, values in grouped:
+        expected[val] = Counter(k for k, _ in values)
+
+    assert expected.keys() == result.keys()
+    for key in expected:
+        result_ctr = Counter(result[key])
+        assert isinstance(result[key], clct)
+        assert expected[key] == result_ctr
+
+
+@given(mapping=valid_mappings(), clct=hu.valid_hashing_collections())
+def test_reverse_multimapping_for_reordering_robust_collections(
+    mapping: dict[int, list[int]],
+    clct: t.Union[
+        t.Type[Counter[int]],
+        t.Type[cc.MyCounter[int]],
+        t.Type[cc.MyFrozenset[int]],
+        t.Type[cc.MySet[int]],
+        t.Type[frozenset[int]],
+        t.Type[set[int]],
+    ],
+) -> None:
+    result = reverse_multimapping(clct, mapping)
+    grouped = groupby(
+        sorted(
+            ((key, val) for key, values in mapping.items() for val in values),
+            key=lambda x: x[1],
+        ),
+        lambda x: x[1],
+    )
+    expected = {}
+    for val, values in grouped:
+        expected[val] = clct(k for k, _ in values)
+    assert result == expected
+
+
+@given(
+    mapping=valid_mappings(),
+    invalid_clct=st.sampled_from([dict, list[int], deque]),
+)
+def test_breaks_for_invalid_collections(
+    mapping: dict[int, list[int]], invalid_clct: t.Type[t.Any]
+) -> None:
+    with pytest.raises(AssertionError):
+        reverse_multimapping(invalid_clct, mapping)
+
+
+def test_breaks_for_unhashable_values() -> None:
+    mapping = {1234: [[1234]]}
+    with pytest.raises(TypeError):
+        reverse_multimapping(set, mapping)  # type: ignore[arg-type, type-var]
+    with pytest.raises(TypeError):
+        reverse_multimapping(frozenset, mapping)  # type: ignore[arg-type, type-var]
+    with pytest.raises(TypeError):
+        reverse_multimapping(cc.MySet, mapping)  # type: ignore[arg-type, type-var]
+    with pytest.raises(TypeError):
+        reverse_multimapping(cc.MyFrozenset, mapping)  # type: ignore[arg-type, type-var]
